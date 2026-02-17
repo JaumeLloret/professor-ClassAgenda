61fc68b575e67a65b91aad0fac441be862b2fcbb
Jaume <elcucala@gmail.com>
2026-02-17 03:02:59 +0100

    Spring 3: Crear el proyecto Maven en IntelliJ, pom.xml completo, primer test y CI basico

---
+++ backend/src/test/java/com/classagendaprofessor/SanityTest.java
@@ -0,0 +1,11 @@
+package com.classagendaprofessor;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SanityTest {
+    @Test
+    void sanity() {
+        assertEquals(2, 1 + 1);
+    }
+}
